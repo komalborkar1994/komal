0360f1b73e9e24ef580c08dbc0d071add1416270
komalborkar1994 <komalborkar0824@gmail.com>
2025-08-15 14:25:11 +0530

    HospitalMangementProject

---
+++ HospitalManegemnetProject/src/main/Dectormain.java
@@ -0,0 +1,13 @@
+package main;
+
+import service.DoctorService;
+
+public class Dectormain {
+	public static void main(String[] args) {
+		DoctorService doctorService=new DoctorService();
+		doctorService.printDentistDetail();
+		
+		
+	}
+
+}
